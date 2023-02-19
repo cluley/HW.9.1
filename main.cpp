ccaec7da76f8526e3a72cefb7c7df4479b2c7b37
Cluley <oo43@yandex.ru>
2023-02-19 22:15:25 +0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+class Fraction
+{
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+	double division() { return static_cast<double>(numerator_) / static_cast<double>(denominator_); }
+	bool operator==(Fraction other) { return division() == other.division(); }
+	bool operator!=(Fraction other) { return !(*this == other); }
+	bool operator<(Fraction other)
+	{
+		if (division() < other.division()) {
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	bool operator>(Fraction other)
+	{
+		if (division() > other.division()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	bool operator<=(Fraction other) { return !(*this > other); }
+	bool operator>=(Fraction other) { return !(*this < other); }
+private:
+	int numerator_;
+	int denominator_;
+};
+
+int main()
+{
+	Fraction f1(4, 3);
+	Fraction f2(6, 11);
+
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+	return 0;
+}
