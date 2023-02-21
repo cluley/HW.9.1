17415617a1fd6cf19db8c1cfcb205c0dcf4db674
Cluley <oo43@yandex.ru>
2023-02-21 16:07:57 +0700

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,36 +1,17 @@
 #include <iostream>
+#include <compare>
 
 class Fraction
 {
 public:
-	Fraction(int numerator, int denominator)
-	{
-		numerator_ = numerator;
-		denominator_ = denominator;
-	}
+	Fraction(int numerator, int denominator) : numerator_(numerator), denominator_(denominator) {}
 	double division() { return static_cast<double>(numerator_) / static_cast<double>(denominator_); }
-	bool operator==(Fraction other) { return division() == other.division(); }
-	bool operator!=(Fraction other) { return !(*this == other); }
-	bool operator<(Fraction other)
-	{
-		if (division() < other.division()) {
-			return true;
-		}
-		else{
-			return false;
-		}
+	auto operator<=>(Fraction other) {
+		return division() <=> other.division();
 	}
-	bool operator>(Fraction other)
-	{
-		if (division() > other.division()) {
-			return true;
-		}
-		else {
-			return false;
-		}
+	bool operator==(Fraction other) {
+		return operator<=>(other) == 0;
 	}
-	bool operator<=(Fraction other) { return !(*this > other); }
-	bool operator>=(Fraction other) { return !(*this < other); }
 private:
 	int numerator_;
 	int denominator_;
@@ -38,8 +19,8 @@ private:
 
 int main()
 {
-	Fraction f1(4, 3);
-	Fraction f2(6, 11);
+	Fraction f1(1, 2);
+	Fraction f2(2, 4);
 
 	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
 	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
