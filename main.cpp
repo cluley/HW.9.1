89fee4d73eaf35b9d5eed67f4eb6bb22c68c0471
Cluley <oo43@yandex.ru>
2023-02-22 23:03:21 +0700

    first amend

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@ public:
 		numerator_ = numerator;
 		denominator_ = denominator;
 	}
-	bool operator==(Fraction other) { return (numerator_ * denominator_) == (other.numerator_ * other.denominator_); }
+	bool operator==(Fraction other) { return (((numerator_ / 1) * (1 / denominator_)) == ((other.numerator_ / 1) * (1 / other.denominator_))); }
 	bool operator!=(Fraction other) { return !(*this == other); }
 	bool operator<(Fraction other)
 	{
-		if ((numerator_ * denominator_) < (other.numerator_ * other.denominator_)) {
+		if (((numerator_ / 1) * (1 / denominator_)) == ((other.numerator_ / 1) * (1 / other.denominator_))) {
 			return true;
 		}
 		else{
